d84736f3b53adc9ca56f779cfe5d921dd6c45cba
Shakhzod Akhmadov <axmedovshaxzod14@gmail.com>
2023-01-12 18:45:50 +0500

    react-pizza

---
+++ react-pizza/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./scss/app.scss";
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+           <PizzaBlock title='Mexico' price='450' />
+           <PizzaBlock title='Americano' price='600' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
